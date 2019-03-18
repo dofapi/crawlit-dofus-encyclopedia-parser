76d22e377af9a931c68e54975c2184b84d94f655
Zakaria RACHEDI <zakaria.rachedi@orange.com>
2019-03-18 15:00:18 +0100

    Merging Error fixed + semantic enhancement done

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+	'env': {
+		'es6': true,
+		'node': true,
+		'mocha': true
+	},
+	'extends': 'eslint:recommended',
+	'globals': {
+		'Atomics': 'readonly',
+		'SharedArrayBuffer': 'readonly'
+	},
+	'parserOptions': {
+		'ecmaVersion': 2018,
+		'sourceType': 'module'
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		],
+		'no-console': 'off',
+		'no-unused-vars': 'off',
+		'no-var': 1,
+		"prefer-const": ["error", {
+			"destructuring": "any",
+			"ignoreReadBeforeAssign": false
+		}],
+		"spaced-comment": ["error", "always", {
+			"line": {
+				"markers": ["/"],
+				"exceptions": ["-", "+"]
+			},
+			"block": {
+				"markers": ["!"],
+				"exceptions": ["*"],
+				"balanced": true
+			}
+		}]
+	}
+};
