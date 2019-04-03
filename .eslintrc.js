aed0eb61ae65d7a14ccfe0e564bbac0a76dbe38e
Zakaria RACHEDI <rachedi.zakaria.idf@gmail.com>
2019-04-03 14:12:38 +0200

    Revert "Add last features from master to refactor"

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,53 +1,52 @@
 module.exports = {
-    'env': {
-        'es6': true,
-        'node': true,
-        'mocha': true
-    },
-    'extends': 'eslint:recommended',
-    'globals': {
-        'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly'
-    },
-    'parserOptions': {
-        'ecmaVersion': 2018,
-        'sourceType': 'module'
-    },
-    'rules': {
-        'indent': [
-            'error',
-            'tab'
-        ],
-        'linebreak-style': [
-            'error',
-            'windows'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ],
-        'no-control-regex': 'off',
-        'no-console': 'off',
-        'no-unused-vars': 'off',
-        'no-var': 1,
-        "prefer-const": ["error", {
-            "destructuring": "any",
-            "ignoreReadBeforeAssign": false
-        }],
-        "spaced-comment": ["error", "always", {
-            "line": {
-                "markers": ["/"],
-                "exceptions": ["-", "+"]
-            },
-            "block": {
-                "markers": ["!"],
-                "exceptions": ["*"],
-                "balanced": true
-            }
-        }]
-    }
+	'env': {
+		'es6': true,
+		'node': true,
+		'mocha': true
+	},
+	'extends': 'eslint:recommended',
+	'globals': {
+		'Atomics': 'readonly',
+		'SharedArrayBuffer': 'readonly'
+	},
+	'parserOptions': {
+		'ecmaVersion': 2018,
+		'sourceType': 'module'
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'windows'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		],
+		'no-console': 'off',
+		'no-unused-vars': 'off',
+		'no-var': 1,
+		"prefer-const": ["error", {
+			"destructuring": "any",
+			"ignoreReadBeforeAssign": false
+		}],
+		"spaced-comment": ["error", "always", {
+			"line": {
+				"markers": ["/"],
+				"exceptions": ["-", "+"]
+			},
+			"block": {
+				"markers": ["!"],
+				"exceptions": ["*"],
+				"balanced": true
+			}
+		}]
+	}
 };
