dofapi/crawlit-dofus-encyclopedia-parser

commit fc2e0560181016dda4aa6b05418be627ffde5376

Remove fs-path dependency

lib/app.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import * as fs from 'fs';
+import fs from 'fs';
 import { getCategory } from './modules/command-line/cmd';
 import errorHandler from './services/errorHandler';
 import { parserProcess, parserInit, getPageLinks } from './modules/global-parser/global-parser';
@@ -9,8 +9,8 @@ import { getLinksFromFile } from './services/utils';
 
 (async function main() {
 	asciiArt();
-	if (process.platform == 'win32') arboInit();
-	const ifResume = fs.existsSync('./data/links/resume.json') ? true : false;
+	if (process.platform === 'win32') arboInit();
+	const ifResume = fs.existsSync('./data/links/resume.json');
 	try {
 		const cmdResponse = JSON.parse(await getCategory(ifResume));
 		if (cmdResponse !== 'yes') {

lib/modules/global-parser/global-parser.js
@@ -1,6 +1,5 @@
 const request = require('request-promise-native');
-import * as fs from 'fs';
-import * as fsPath from 'fs-path';
+import fs from 'fs';
 import getItems from './getItems';
 import { cmdSwitch, cmdSwitchEn } from '../command-line/conf/cmdSwitch';
 import { Spinner } from 'clui';
@@ -15,7 +14,7 @@ async function parserProcess(category, links, game) {
 		category = category.replace(/ /g, '');
 		if (fs.existsSync('./data/links/resume.json')) fs.unlinkSync('./data/links/resume.json');
 		if (fs.existsSync('./data/links/' + category + '_links.json')) fs.unlinkSync('./data/links/' + category + '_links.json');
-		fsPath.writeFileSync('./data/' + game + '/' + category + '.json', JSON.stringify(items, null, 4));
+		fs.writeFileSync('./data/' + game + '/' + category + '.json', JSON.stringify(items, null, 4));
 		mongoImport(category, game);
 		return { 'numberItemParsed': items.length, 'categoryName': category };
 	} catch (err) {
@@ -56,7 +55,7 @@ async function getPageLinks() {
 		});
 	}
 
-	fsPath.writeFileSync('./data/links/' + itemCategory + '_links.json', JSON.stringify(links));
+	fs.writeFileSync('./data/links/' + itemCategory + '_links.json', JSON.stringify(links));
 	console.log('\x1b[36m%s\x1b[0m', '\n SUCCESS : all item(s) links crawled.');
 	console.log('\x1b[36m%s\x1b[0m', '\n START of item(s) crawling.');
 }

lib/modules/global-parser/services/parser-backup.js
@@ -1,11 +1,10 @@
-import * as fs from 'fs';
-import * as fsPath from 'fs-path';
+import fs from 'fs';
 
 function saveData(category, lastIndex, links, items, game) {
 	category = category.replace(/ /g, '');
 	const resume = resumeFileStructure(category, lastIndex, links, game);
-	fsPath.writeFileSync('./data/' + game + '/' + category + '.json', JSON.stringify(items));
-	fsPath.writeFileSync('./data/links/resume.json', JSON.stringify(resume));
+	fs.writeFileSync('./data/' + game + '/' + category + '.json', JSON.stringify(items));
+	fs.writeFileSync('./data/links/resume.json', JSON.stringify(resume));
 	console.log('\x1b[33m%s\x1b[0m', '\n INFO : ' + items.length + ' item(s) were crawled.');
 }
 

lib/modules/global-parser/services/utils.js
@@ -1,5 +1,4 @@
-import * as fs from 'fs';
-import * as fsPath from 'fs-path';
+import fs from 'fs';
 import cheerio from 'cheerio';
 import errorHandler from '../../../services/errorHandler';
 import globalConf from '../../../conf/global.conf';
@@ -19,7 +18,7 @@ function mongoImport(category, game) {
 		items = items.replace(/,{"_id"/g, '{"_id"');
 		items = items.replace(/,null/g, '');
 		items = items.substring(1, items.length - 1);
-		fsPath.writeFileSync(process.cwd() + '/data/' + game + '/mongo/' + category + '_mongo.json', items);
+		fs.writeFileSync(process.cwd() + '/data/' + game + '/mongo/' + category + '_mongo.json', items);
 	} catch (error) {
 		new errorHandler(error);
 	}

lib/services/utils.js
@@ -1,5 +1,4 @@
-import * as fsPath from 'fs-path';
-import * as fs from 'fs';
+import fs from 'fs';
 import errorHandler from '../services/errorHandler';
 
 export function asciiArt() {
@@ -27,14 +26,15 @@ export function wait(ms) {
 }
 
 export function arboInit() {
-	if (!fs.existsSync(process.cwd() + '/data/dofus-touch') && !fs.existsSync(process.cwd() + '/data/dofus') && !fs.existsSync(process.cwd() + '/data/links')) {
-		fsPath.mkdirSync(process.cwd() + '/data');
-		fsPath.mkdirSync(process.cwd() + '/data/dofus-touch');
-		fsPath.mkdirSync(process.cwd() + '/data/dofus');
-		fsPath.mkdirSync(process.cwd() + '/data/wakfu');
-		fsPath.mkdirSync(process.cwd() + '/data/waven');
-		fsPath.mkdirSync(process.cwd() + '/data/links');
-	}
+    mkdirSync(process.cwd() + '/data/dofus-touch', {recursive: true});
+    mkdirSync(process.cwd() + '/data/dofus', {recursive: true});
+    mkdirSync(process.cwd() + '/data/wakfu', {recursive: true});
+    mkdirSync(process.cwd() + '/data/waven', {recursive: true});
+    mkdirSync(process.cwd() + '/data/links', {recursive: true});
+}
+
+function mkdirSync(path, options) {
+    if (!fs.existsSync(path)) fs.mkdirSync(path, options); 
 }
 
 export function getLinksFromFile(itemCategory) {

commit f332f137ad872aee0f2e3ff04937c6b4fd79f295

Fix path error on linux caused by fc2e0560

lib/app.js
@@ -9,7 +9,7 @@ import { getLinksFromFile } from './services/utils';
 
 (async function main() {
 	asciiArt();
-	if (process.platform === 'win32') arboInit();
+	arboInit();
 	const ifResume = fs.existsSync('./data/links/resume.json');
 	try {
 		const cmdResponse = JSON.parse(await getCategory(ifResume));